d789667cf5a633daf4fbdc02e3f06459de2649d0
Namazi Mohammed Jamal <82827087+Jamal-2002@users.noreply.github.com>
2025-04-22 10:32:55 +0530

    Add files via upload

---
+++ 49. Group Anagrams.py
@@ -0,0 +1,50 @@
+"""
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+Example 2:
+
+Input: strs = [""]
+
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+
+Output: [["a"]]
+
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+"""
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        res = defaultdict(list)
+
+        for word in strs:
+            count = [0] * 26
+    
+            for letter in word:
+                count[ord(letter) - ord("a")] += 1
+    
+            res[tuple(count)].append(word)
+
+        return res.values()
